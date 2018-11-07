620fcb626a00c9683be95e87940ba6f9e7a773ac
srafu <saraf.arul@utexas.edu>
2018-11-07 16:52:14 -0600

    Set up function templates

---
+++ deck.cpp
@@ -0,0 +1,18 @@
+#include "card.h"
+#include "deck.h"
+
+static const int SIZE = 52;
+Card myCards[SIZE];
+int myIndex; 
+
+Deck::Deck(){
+}
+
+void Deck::shuffle(){
+}
+
+Card Deck::dealCard(){
+}
+
+int Deck::size() const{
+}
